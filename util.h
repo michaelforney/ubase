986e4981dfbb75ec03ece57b7632523f368f0110
sin <sin@2f30.org>
2013-08-29 11:14:00 +0100

    No need to name the parameters in the function decls

--- util.h
+++ util.h
@@ -8,10 +8,10 @@ extern char *argv0;
 
 char *agetcwd(void);
 void apathmax(char **, long *);
-void devtotty(int dev, int *tty_maj, int *tty_min);
+void devtotty(int, int *, int *);
 void enprintf(int, const char *, ...);
 void eprintf(const char *, ...);
 long estrtol(const char *, int);
 void putword(const char *);
 void recurse(const char *, void (*)(const char *));
-char *ttytostr(int tty_maj, int tty_min);
+char *ttytostr(int, int);

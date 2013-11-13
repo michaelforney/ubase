a42f9093621848ccf7a7849e041713374e679781
sin <sin@2f30.org>
2013-11-13 11:47:05 +0000

    Rename `brk' to `_brk' to avoid polluting the namespace

--- arg.h
+++ arg.h
@@ -15,14 +15,14 @@ extern char *argv0;
 					argc--, argv++) {\
 				char _argc;\
 				char **_argv;\
-				int brk;\
+				int _brk;\
 				if (argv[0][1] == '-' && argv[0][2] == '\0') {\
 					argv++;\
 					argc--;\
 					break;\
 				}\
-				for (brk = 0, argv[0]++, _argv = argv;\
-						argv[0][0] && !brk;\
+				for (_brk = 0, argv[0]++, _argv = argv;\
+						argv[0][0] && !_brk;\
 						argv[0]++) {\
 					if (_argv != argv)\
 						break;\
@@ -36,13 +36,13 @@ extern char *argv0;
 
 #define EARGF(x)	((argv[0][1] == '\0' && argv[1] == NULL)?\
 				((x), abort(), (char *)0) :\
-				(brk = 1, (argv[0][1] != '\0')?\
+				(_brk = 1, (argv[0][1] != '\0')?\
 					(&argv[0][1]) :\
 					(argc--, argv++, argv[0])))
 
 #define ARGF()		((argv[0][1] == '\0' && argv[1] == NULL)?\
 				(char *)0 :\
-				(brk = 1, (argv[0][1] != '\0')?\
+				(_brk = 1, (argv[0][1] != '\0')?\
 					(&argv[0][1]) :\
 					(argc--, argv++, argv[0])))
 

1e78b0aa160b23559565cffd8678fc38af3f3a41
sin <sin@2f30.org>
2014-04-14 14:44:58 +0100

    Simplify config.mk and update README for tcc

--- config.mk
+++ config.mk
@@ -8,12 +8,6 @@ MANPREFIX = $(PREFIX)/share/man
 #CC = gcc
 #CC = musl-gcc
 LD = $(CC)
-CPPFLAGS = -D_BSD_SOURCE -D_GNU_SOURCE
-CFLAGS   = -g -std=c99 -Wall -Wextra $(CPPFLAGS)
-LDFLAGS  = -g -lcrypt
-
-#CC = tcc
-#LD = $(CC)
-#CPPFLAGS = -D_BSD_SOURCE -D_GNU_SOURCE
-#CFLAGS   = -Os -Wall $(CPPFLAGS)
-#LDFLAGS  = -lcrypt
+CPPFLAGS = -D_GNU_SOURCE
+CFLAGS   = -Os -Wall -Wextra $(CPPFLAGS)
+LDFLAGS  = -lcrypt

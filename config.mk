1b28deba3dc648cd2a67c9ea6f8db26fee416fec
sin <sin@2f30.org>
2014-06-04 15:08:01 +0100

    Add -D_FILE_OFFSET_BITS=64 to config.mk
    stat() will fail if compiled on a 32-bit platform and the file
    size is greater than (1<<31)-1 bytes.

--- config.mk
+++ config.mk
@@ -8,6 +8,6 @@ MANPREFIX = $(PREFIX)/share/man
 #CC = gcc
 #CC = musl-gcc
 LD = $(CC)
-CPPFLAGS = -D_GNU_SOURCE
+CPPFLAGS = -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64
 CFLAGS   = -std=c99 -Wall -Wextra $(CPPFLAGS)
 LDFLAGS  = -s -lcrypt # -static

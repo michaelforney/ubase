55dbbb7b3f726aa6fb836961f35d6128b6aa8adb
sin <sin@2f30.org>
2014-06-30 16:50:29 +0100

    Use estrtol() instead of atoi()
    Some more stylistic fixes.

--- chvt.c
+++ chvt.c
@@ -1,10 +1,11 @@
+/* See LICENSE file for copyright and license details. */
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "util.h"
 
 #define KDGKBTYPE	0x4B33	/* get keyboard type */
@@ -32,25 +33,25 @@ main(int argc, char *argv[])
 	int fd;
 	char c;
 
-	if(argc!=2 || strspn(argv[1], "1234567890") != strlen(argv[1]))
+	if (argc != 2 || strspn(argv[1], "1234567890") != strlen(argv[1]))
 		usage();
 
-	n = atoi(argv[1]);
-	for(i = 0; i < LEN(vts); i++) {
+	n = estrtol(argv[1], 10);
+	for (i = 0; i < LEN(vts); i++) {
 		fd = open(vts[i], O_RDONLY);
-		if(fd < 1)
+		if (fd < 1)
 			continue;
 		c = 0;
-		if(ioctl(fd, KDGKBTYPE, &c) == 0)
+		if (ioctl(fd, KDGKBTYPE, &c) == 0)
 			goto VTfound;
 		close(fd);
 	}
 
 	eprintf("couldn't find a console.\n");
 VTfound:
-	if(ioctl(fd, VT_ACTIVATE, n) == -1)
+	if (ioctl(fd, VT_ACTIVATE, n) == -1)
 		eprintf("VT_ACTIVATE %d:", n);
-	if(ioctl(fd, VT_WAITACTIVE, n) == -1)
+	if (ioctl(fd, VT_WAITACTIVE, n) == -1)
 		eprintf("VT_WAITACTIVE %d:", n);
 	close(fd);
 
